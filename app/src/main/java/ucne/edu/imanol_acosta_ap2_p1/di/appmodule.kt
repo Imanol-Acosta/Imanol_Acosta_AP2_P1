b39a3fa28a79a362ae7adf64c76749e1a451285f
Imanol-Acosta <ilacosta05@hotmail.com>
2026-02-04 20:12:23 -0400

    Agregando appmodule.kt

---
+++ app/src/main/java/ucne/edu/imanol_acosta_ap2_p1/di/appmodule.kt
@@ -0,0 +1,11 @@
+package ucne.edu.imanol_acosta_ap2_p1.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule{
+
+}
