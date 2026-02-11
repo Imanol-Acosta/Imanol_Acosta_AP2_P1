0af81d8f6ef07a87d24516ee36446f8b9acf86ce
Imanol-Acosta <ilacosta05@hotmail.com>
2026-02-11 18:59:18 -0400

    Agregando BorrameEntity.kt con datos para cerveza

--- app/src/main/java/ucne/edu/imanol_acosta_ap2_p1/data/local/entity/BorrameEntity.kt
+++ app/src/main/java/ucne/edu/imanol_acosta_ap2_p1/data/local/entity/BorrameEntity.kt
@@ -3,11 +3,11 @@ package ucne.edu.imanol_acosta_ap2_p1.data.local.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "Borrame")
+@Entity(tableName = "Cerveza")
 data class BorrameEntity(
     @PrimaryKey(autoGenerate = true)
-    val borrameId: Int = 0,
+    val cervezaId: Int = 0,
     val nombre: String = "",
-    val descripcion: String = "",
-    val valor: Int = 0
+    val marca: String = "",
+    val puntuacion: Int = 0
 )
