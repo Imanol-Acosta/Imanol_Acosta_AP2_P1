e7ac9dc40fc131af72b577a35b448a15817ce66d
Imanol-Acosta <ilacosta05@hotmail.com>
2026-02-11 18:57:18 -0400

    Agregando BorrameDao.kt

--- app/src/main/java/ucne/edu/imanol_acosta_ap2_p1/data/local/dao/BorrameDao.kt
+++ app/src/main/java/ucne/edu/imanol_acosta_ap2_p1/data/local/dao/BorrameDao.kt
@@ -1,9 +1,24 @@
 package ucne.edu.imanol_acosta_ap2_p1.data.local.dao
 
+import ucne.edu.imanol_acosta_ap2_p1.data.local.entity.BorrameEntity
 import androidx.room.Dao
-git
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface BorrameDao {
+    @Query("SELECT * FROM Cerveza ORDER BY CervezaId DESC")
+    fun observeAll(): Flow<List<BorrameEntity>>
+
+    @Query("SELECT * FROM Cerveza WHERE CervezaId = :id")
+    suspend fun getById(id: Int): BorrameEntity?
+
+    @Upsert
+    suspend fun upsert(borrame: BorrameEntity): Long
+
+    @Delete
+    suspend fun delete(borrame: BorrameEntity)
 
 }
