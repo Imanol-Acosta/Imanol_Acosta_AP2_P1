6f90e5a6fd146f6be80b8415659ba3b285f6ac2c
Imanol-Acosta <ilacosta05@hotmail.com>
2026-02-11 18:57:57 -0400

    Agregando Borrame.kt con valores de cerveza

--- app/src/main/java/ucne/edu/imanol_acosta_ap2_p1/domain/Model/Borrame.kt
+++ app/src/main/java/ucne/edu/imanol_acosta_ap2_p1/domain/Model/Borrame.kt
@@ -1,8 +1,7 @@
 package ucne.edu.imanol_acosta_ap2_p1.domain.Model
 
 class Borrame(
-    val borrameId: Int = 0,
+    val cervezaId: Int = 0,
     val nombre: String = "",
-    val descripcion: String = "",
-    val valor: Int = 0
+    val puntuacion: Int = 0,
 )
