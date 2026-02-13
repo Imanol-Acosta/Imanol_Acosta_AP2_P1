75afa0d49acba8437678fa27611266f37a03596a
Imanol-Acosta <ilacosta05@hotmail.com>
2026-02-12 22:17:47 -0400

    Arreglando y modificando MainActivity.kt

--- app/src/main/java/ucne/edu/imanol_acosta_ap2_p1/MainActivity.kt
+++ app/src/main/java/ucne/edu/imanol_acosta_ap2_p1/MainActivity.kt
@@ -3,9 +3,12 @@ package ucne.edu.imanol_acosta_ap2_p1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import dagger.hilt.android.AndroidEntryPoint
-import ucne.edu.imanol_acosta_ap2_p1.presentation.Navigation.BorrameNavHost
+import ucne.edu.imanol_acosta_ap2_p1.presentation.Navigation.CervezaNavHost
 import ucne.edu.imanol_acosta_ap2_p1.ui.theme.Imanol_Acosta_AP2_P1Theme
 
 @AndroidEntryPoint
@@ -15,7 +18,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             Imanol_Acosta_AP2_P1Theme {
                 val navController = rememberNavController()
-                BorrameNavHost(navController)
+
+                CervezaNavHost(
+                    navHostController = navController
+                )
             }
         }
     }
