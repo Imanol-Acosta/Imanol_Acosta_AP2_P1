3b934f50098a91b2e33f08d7ab5647cc3a004751
Imanol-Acosta <ilacosta05@hotmail.com>
2026-02-12 21:50:17 -0400

    Eliminando archivos innecesarios

--- app/src/main/java/ucne/edu/imanol_acosta_ap2_p1/domain/Model/Borrame.kt
+++
@@ -1,8 +0,0 @@
-package ucne.edu.imanol_acosta_ap2_p1.domain.Model
-
-class Borrame(
-    val cervezaId: Int = 0,
-    val nombre: String = "",
-    val marca: String = "",
-    val puntuacion: Int = 0,
-)
