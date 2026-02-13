75241614bf4467d396324c31d69cb8ec8b951a78
Imanol-Acosta <ilacosta05@hotmail.com>
2026-02-12 22:14:23 -0400

    Agregando archivos de list

---
+++ app/src/main/java/ucne/edu/imanol_acosta_ap2_p1/presentation/cerveza/List/ListCervezaUiState.kt
@@ -0,0 +1,11 @@
+package ucne.edu.imanol_acosta_ap2_p1.presentation.cerveza.List
+
+import ucne.edu.imanol_acosta_ap2_p1.domain.Model.Cerveza
+
+data class ListCervezaUiState(
+    val isLoading: Boolean = false,
+    val cervezas: List<Cerveza> = emptyList(),
+    val message: String? = null,
+    val navigateToCreate: Boolean = false,
+    val navigateToEditId: Int? = null
+)
